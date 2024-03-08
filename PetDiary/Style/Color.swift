d63e7515027baa4bb6c0b03ef2fbd13b34e3dc73
Minchelin42 <mje0211@naver.com>
2024-03-09 00:51:52 +0900

    DESIGN: Color 세팅

---
+++ PetDiary/Style/Color.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  PetDiary
+//
+//  Created by 민지은 on 2024/03/08.
+//
+
+import UIKit
+
+enum Color {
+    static let lightGreen = UIColor(named: "lightGreen")
+    static let darkGreen = UIColor(named: "darkGreen")
+    static let green = UIColor(named: "green")
+    static let lightGray = UIColor(named: "lightGray")
+    static let darkGray = UIColor(named: "darkGray")
+    static let font = UIColor(named: "font")
+    static let background = UIColor(named: "background")
+    
+    
+}
